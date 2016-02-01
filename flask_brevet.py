9c2b68706772027b2dd313c8b28cf30d120a3092
lbsilver <lb_silver@hotmail.com>
2016-02-01 15:22:27 -0800

    update

--- flask_brevet.py
+++ flask_brevet.py
@@ -1,28 +1,39 @@
 """
-Simple Flask web site 
+Very simple Flask web site, with one page
+displaying a course schedule.
 
-Edited by Omar Alamoudi
 """
 
 import flask
-# from flask import render_template
-from flask import request  # Data from a submitted form
+from flask import render_template
+from flask import request
 from flask import url_for
 from flask import jsonify # For AJAX transactions
 
 import json
 import logging
-import argparse  # For the vocabulary list
-import arrow
-import datetime
-from dateutil import tz
+
+# Date handling 
+import arrow # Replacement for datetime, based on moment.js
+import datetime # But we still need time
+from dateutil import tz  # For interpreting local times
+
+# Our own module
+# import acp_limits
+
+import brevet
 
 ###
 # Globals
 ###
 app = flask.Flask(__name__)
 import CONFIG
-app.secret_key = CONFIG.COOKIE_KEY  # Should allow using session variables
+
+import uuid
+app.secret_key = str(uuid.uuid4())
+app.debug=CONFIG.DEBUG
+app.logger.setLevel(logging.DEBUG)
+
 
 ###
 # Pages
@@ -30,68 +41,68 @@ app.secret_key = CONFIG.COOKIE_KEY  # Should allow using session variables
 
 @app.route("/")
 @app.route("/index")
-@app.route("/brevet")
+@app.route("/calc")
 def index():
-    
-    return flask.render_template('calc.html')
-
-  
+  app.logger.debug("Main page entry")
+  return flask.render_template('calc.html')
 
 
-
-
-  
-
-
-#################
-# Functions used within the templates
-#################
-
-@app.template_filter( 'filt' )
-def format_filt( something ):
-    """
-    Example of a filter that can be used within
-    the Jinja2 code
-    """
-    return "Not what you asked for"
-  
-###################
-#   Error handlers
-###################
 @app.errorhandler(404)
-def error_404(e):
-  app.logger.warning("++ 404 error: {}".format(e))
-  return render_template('404.html'), 404
+def page_not_found(error):
+    app.logger.debug("Page not found")
+    flask.session['linkback'] =  flask.url_for("calc")
+    return flask.render_template('page_not_found.html'), 404
 
-@app.errorhandler(500)
-def error_500(e):
-   app.logger.warning("++ 500 error: {}".format(e))
-   assert app.debug == False #  I want to invoke the debugger
-   return render_template('500.html'), 500
 
-@app.errorhandler(403)
-def error_403(e):
-  app.logger.warning("++ 403 error: {}".format(e))
-  return render_template('403.html'), 403
+###############
+#
+# AJAX request handlers 
+#   These return JSON, rather than rendering pages. 
+#
+###############
+@app.route("/_calc_times")
+def calc_times():
+  """
+  Calculates open/close times from miles, using rules 
+  described at http://www.rusa.org/octime_alg.html.
+  Expects one URL-encoded argument, the number of miles. 
+  """
+  app.logger.debug("Got a JSON request");
+  miles = request.args.get('miles', 0, type=int)
+  return jsonify(result=miles * 2)
+ 
+#################
+#
+# Functions used within the templates
+#
+#################
+
+@app.template_filter( 'fmtdate' )
+def format_arrow_date( date ):
+    try: 
+        normal = arrow.get( date )
+        return normal.format("ddd MM/DD/YYYY")
+    except:
+        return "(bad date)"
+
+@app.template_filter( 'fmttime' )
+def format_arrow_time( time ):
+    try: 
+        normal = arrow.get( date )
+        return normal.format("hh:mm")
+    except:
+        return "(bad time)"
 
 
 
 #############
 
-# Set up to run from cgi-bin script, from
-# gunicorn, or stand-alone.
-#
 
 if __name__ == "__main__":
-    # Standalone. 
-    app.debug = True
+    import uuid
+    app.secret_key = str(uuid.uuid4())
+    app.debug=CONFIG.DEBUG
     app.logger.setLevel(logging.DEBUG)
-    print("Opening for global access on port {}".format(CONFIG.PORT))
-    app.run(port=CONFIG.PORT, host="0.0.0.0")
-else:
-    # Running from cgi-bin or from gunicorn WSGI server, 
-    # which makes the call to app.run.  Gunicorn may invoke more than
-    # one instance for concurrent service.
-    #FIXME:  Debug cgi interface 
-    app.debug=False
+    app.run(port=CONFIG.PORT)
 
+    
